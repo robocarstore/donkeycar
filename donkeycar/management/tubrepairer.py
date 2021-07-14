5ed5fa52433c11856e87e4f85fc4fae195a4f934
kingeugeneIV <47935524+Eugene-Lam@users.noreply.github.com>
2021-07-14 14:03:14 +0800

    delete 0 byte manifest

--- donkeycar/management/tubrepairer.py
+++ donkeycar/management/tubrepairer.py
@@ -29,8 +29,7 @@ class TubRepairer(object):
 
         for _ in tub_paths:
             self.tub = Tub(_)
-            md, useless_index = self.__cleansing()
-            # if health == 0: continue
+            md, useless_index, health = self.__cleansing()
             self.tub.delete_records(useless_index)
             self.__delete_zero_byte_image()
             self.tub.update_catalog(catalog=md)
@@ -39,7 +38,7 @@ class TubRepairer(object):
         import glob
         _ = glob.glob(f'{self.tub.base_path}/catalog_*.catalog')
         tem = [path.split(x)[-1] for x in _ if path.getsize(x) != 0]
-        var = {remove(x) for x in _ if path.getsize(x) == 0}
+
         dir_image = listdir(self.tub.images_base_path)
 
         def __getZero(_):
@@ -48,10 +47,10 @@ class TubRepairer(object):
 
         zero = __getZero(dir_image)
 
-        # if len(zero) == 0:  # health check
-        #     return sorted(tem), '', 0
+        # if len(sorted(tem)) == 0 and len(zero) == 0:  # health check
+        #     return '', '', 0
 
-        return sorted(tem), zero
+        return sorted(tem), zero, 1
 
     def __delete_zero_byte_image(self):
         try:
