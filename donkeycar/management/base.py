974d672dfc2fc33b2892b5d350c74584c720674f
kingeugeneIV <47935524+Eugene-Lam@users.noreply.github.com>
2021-07-14 11:50:23 +0800

    check 0 byte data and remove it

--- donkeycar/management/base.py
+++ donkeycar/management/base.py
@@ -1,4 +1,3 @@
-
 import argparse
 import os
 import shutil
@@ -38,7 +37,6 @@ def make_dir(path):
 
 
 def load_config(config_path):
-
     '''
     load a config from the given path
     '''
@@ -694,6 +692,29 @@ class Gui(BaseCommand):
         main()
 
 
+class TubRepairShell(BaseCommand):
+    """
+    Remove 0 byte image and update catalog
+    """
+    @staticmethod
+    def parse_args(args):
+        parser = argparse.ArgumentParser(prog='tubrepair')
+        parser.add_argument('--tub', help='The tub to be repair')
+        parsed_args = parser.parse_args(args)
+        return parsed_args, parser
+
+    def run(self, args):
+        """
+        Remove 0 byte image and update catalog
+        """
+        args, parser = self.parse_args(args)
+
+        from donkeycar.management.tubrepairer import TubRepairer
+
+        tr = TubRepairer(args, parser)
+        tr.run()
+
+
 def execute_from_command_line():
     """
     This is the function linked to the "donkey" terminal command.
@@ -710,10 +731,11 @@ def execute_from_command_line():
         'cnnactivations': ShowCnnActivations,
         'update': UpdateCar,
         'train': Train,
-        'trainremote': TrainRemote, 
+        'trainremote': TrainRemote,
         'ui': Gui,
+        'tubrepair': TubRepairShell,
     }
-    
+
     args = sys.argv[:]
 
     if len(args) > 1 and args[1] in commands.keys():
@@ -724,6 +746,6 @@ def execute_from_command_line():
         dk.utils.eprint('Usage: The available commands are:')
         dk.utils.eprint(list(commands.keys()))
 
-    
+
 if __name__ == "__main__":
     execute_from_command_line()
---
+++ donkeycar/management/tubrepairer.py
@@ -0,0 +1,56 @@
+from donkeycar.parts.tub_v2 import Tub
+from os import listdir, path, remove
+import re
+
+
+class Tub(Tub):
+    def update_catalog(self, catalog):
+        self.manifest.catalog_paths = catalog
+        self.manifest._update_catalog_metadata(self)
+
+
+class TubRepairer(object):
+
+    def __init__(self, args, parser):
+        self.args = args
+        self.parser = parser
+        self.tub = Tub(args.tub)
+
+    def run(self):
+        """
+        Remove some 0 byte image and update catalog
+        """
+        if self.args.tub is None:
+            print("ERR>> --tub argument missing.")
+            self.parser.print_help()
+            return
+
+        md, useless_index, health = self.__cleansing()
+        if health == 0: return 'Tub is health'
+        self.tub.delete_records(useless_index)
+        self.__delete_zero_byte_image()
+        self.tub.update_catalog(catalog=md)
+
+    def __cleansing(self):
+        import glob
+        _ = glob.glob(f'{self.tub.base_path}/catalog_*.catalog')
+        tem = [x.replace(f'{self.tub.base_path}/', '') for x in _ if path.getsize(x) != 0]
+
+        dir_image = listdir(self.tub.images_base_path)
+
+        def __getZero(_):
+            return {int(re.findall('\d{1,}', x)[0]) for x in dir_image if
+                    path.getsize(f'{self.tub.images_base_path}/{x}') == 0}
+
+        zero = __getZero(dir_image)
+
+        if len(sorted(tem)) == 0 and len(zero) == 0:  # health check
+            return '', '', 0
+
+        return sorted(tem), zero, 1
+
+    def __delete_zero_byte_image(self):
+        try:
+            {remove(f'{self.tub.images_base_path}/{x}_cam_image_array_.jpg') for x in self.tub.manifest.deleted_indexes}
+        except:
+            print('Nothing to remove')
