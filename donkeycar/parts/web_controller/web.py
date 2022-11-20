e26bf8c466944dfae5dfb6dd47ea711f5c9bed12
Eugene Lam <47935524+Eugene-Lam@users.noreply.github.com>
2022-11-20 14:11:35 +0800

    Revert "Update web.py"

--- donkeycar/parts/web_controller/web.py
+++ donkeycar/parts/web_controller/web.py
@@ -186,16 +186,13 @@ class LocalWebController(tornado.web.Application):
             self.mode = self.mode_latch
             self.mode_latch = None
             changes["driveMode"] = self.mode
-#         if recording is not None and self.recording != recording:
-#             self.recording = recording
-#             changes["recording"] = self.recording
-#         if self.recording_latch is not None:
-#             self.recording = self.recording_latch;
-#             self.recording_latch = None;
-#             changes["recording"] = self.recording;
-        changes["recording"] = self.recording;
-        changes["throttle"] = self.throttle
-        changes["angle"] = self.angle
+        if recording is not None and self.recording != recording:
+            self.recording = recording
+            changes["recording"] = self.recording
+        if self.recording_latch is not None:
+            self.recording = self.recording_latch;
+            self.recording_latch = None;
+            changes["recording"] = self.recording;
 
         # Send record count to websocket clients
         if (self.num_records is not None and self.recording is True):
